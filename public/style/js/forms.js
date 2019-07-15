bcddb4b1d64831f6f9ee5f2233bf39f9abac40a8
baglansariev <baglansariev@mail.ru>
2019-07-15 16:20:07 +0600

    Валидация формы регистрации no:1

--- public/style/js/forms.js
+++ public/style/js/forms.js
@@ -3,6 +3,7 @@ $(function () {
     // Валидация формы регистрации
     $('#register_submit').click(function (e){
        e.preventDefault();
+       $('.form-message-alert').remove();
 
        var place_name = $('[name="place_name"]');
        var user_fullname = $('[name="user_fullname"]');
@@ -16,34 +17,19 @@ $(function () {
        }
        else{
            if(!place_name.val()){
-               !$('.form-message-alert')[0]? place_name.before('<span class="form-message-alert">Поле "название заведения" не должно быть пустым</span>'): $('.form-message-alert')[0].remove();
-           }
-           else{
-               $('.form-message-alert')[0].remove();
+               place_name.before('<span class="form-message-alert">Поле "название заведения" не должно быть пустым</span>');
            }
            if(!user_fullname.val()){
-               !$('.form-message-alert')[1]? user_fullname.before('<span class="form-message-alert">Поле "Ф.И.О. ответственного лица" не должно быть пустым</span>'): $('.form-message-alert')[1].remove();
-           }
-           else{
-               $('.form-message-alert')[1].remove();
+               user_fullname.before('<span class="form-message-alert">Поле "Ф.И.О. ответственного лица" не должно быть пустым</span>');
            }
            if(!user_email.val()){
-               !$('.form-message-alert')[2]? user_email.before('<span class="form-message-alert">Поле "E-mail" не должно быть пустым</span>'): $('.form-message-alert')[2].remove();
-           }
-           else{
-               $('.form-message-alert')[2].remove();
+               user_email.before('<span class="form-message-alert">Поле "E-mail" не должно быть пустым</span>');
            }
            if(!user_password.val()){
-               !$('.form-message-alert')[3]? user_password.before('<span class="form-message-alert">Поле "Пароль" не должно быть пустым</span>'): $('.form-message-alert')[3].remove();
-           }
-           else{
-               $('.form-message-alert')[3].remove();
+              user_password.before('<span class="form-message-alert">Поле "Пароль" не должно быть пустым</span>');
            }
            if(!user_confirm.val()){
-               !$('.form-message-alert')[4]? user_confirm.before('<span class="form-message-alert">Поле "Подтверждение пароля" не должно быть пустым</span>'): $('.form-message-alert')[4].remove();
-           }
-           else{
-               $('.form-message-alert')[4].remove();
+              user_confirm.before('<span class="form-message-alert">Поле "Подтверждение пароля" не должно быть пустым</span>');
            }
        }
     });
